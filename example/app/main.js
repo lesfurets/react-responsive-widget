6ee3d7f56e6db454a837aaee714fcb781cb59934
svi <sylvain.vincent@lesfurets.com>
2018-08-29 10:17:41 +0200

    Migration Bootstrap 4 step 1

--- example/app/main.js
+++ example/app/main.js
@@ -3,6 +3,5 @@ import ReactDom from "react-dom";
 
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById('app'))
-// ReactDom.render(<App />, document.getElementById('app2'))
+ReactDom.render(<App />, document.getElementById('AppContainer'))
 
